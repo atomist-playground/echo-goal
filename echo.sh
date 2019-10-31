f587b329e7d3cc15c161ab0fdb10cf4a14765895
Christian Dupuis <cd@atomist.com>
2019-10-31 10:47:32 +0100

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -9,8 +9,8 @@ cat $ATOMIST_GOAL
 echo "<<<<<<<<<<<<<<<<<<< ---- >>>>>>>>>>>>>>>>>>>>"
 
 echo "<<<<<<<<<<<<<<<<<<< SECRETS >>>>>>>>>>>>>>>>>>>>"
-# cat $ATOMIST_SECRETS
-echo "<hidden>"
+cat $ATOMIST_SECRETS
+#echo "<hidden>"
 echo "<<<<<<<<<<<<<<<<<<< ------- >>>>>>>>>>>>>>>>>>>>"
 
 echo '{ "state": "waiting_for_approval", "description": "Finished echo goal" }' > $ATOMIST_RESULT
